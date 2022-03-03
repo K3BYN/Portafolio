1663e297bfadbde03fa01631f8b07821744bfc7d
K3BIN <71346566+K3BIN@users.noreply.github.com>
2022-03-03 17:36:11 -0600

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,28 +1,33 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default MyDocument = () => {
-  
-  return (
-    <Html>
-      <Head>
-        <title> My Portfolio </title>
-        <link rel="shortcut icon" href="icon.jpg" />
-        <meta
-          name="description"
-          content="Kevin Hernández Portfolio website"
-        />
-      </Head>
-      <body>
-        
-          <Main />
-          <NextScript />
-        
-      </body>
-    </Html>
-  );
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return {...initialProps}
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title> My Portfolio </title>
+          <link rel="shortcut icon" href="icon.jpg" />
+          <meta
+            name="description"
+            content="Kevin Hernández Portfolio website"
+          />
+        </Head>
+        <body>
+          
+            <Main />
+            <NextScript />
+          
+        </body>
+      </Html>
+    );
+  }
 }
 
-MyDocument.getInitialProps =  async(ctx) => {
-  const initialProps = await Document.getInitialProps(ctx)
-  return {...initialProps}
-}
+export default MyDocument
